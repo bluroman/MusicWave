eff126d6f364bf3f81a9741dccea66919ecdc4ac
hun nam <bluroman@hun-nam-ui-MacBook.local>
2011-07-14 17:19:59 +0900

    Initial Commit

---
+++ MusicWave/MusicWaveAppDelegate.h
@@ -0,0 +1,26 @@
+//
+//  MusicWaveAppDelegate.h
+//  MusicWave
+//
+//  Created by hun nam on 11. 7. 14..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusicWaveAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
