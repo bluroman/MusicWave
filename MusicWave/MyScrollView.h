2835dd47a18e7dbaf9d1b02ea0d73994258cbdab
Hoon, Nam <bluroman@gmail.com>
2013-01-19 14:10:29 +0900

    everything move to scrollview

--- MusicWave/MyScrollView.h
+++ MusicWave/MyScrollView.h
@@ -7,10 +7,40 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BookMarkLayerDelegate.h"
+#import "Song.h"
 
+@class GraphViewSegment;
+@class BookMark;
 
 @interface MyScrollView : UIScrollView {
+    CGContextRef context;
+    CGPoint touchPoint;
+    UIViewController * parent;
+    CGFloat currentPixel;
+    Song *currentSong;
     
+    
+    CALayer *bookMarkLayer;
+    BookMarkLayerDelegate *bookMarkLayerDelegate;
+    UIImageView *graphImageView;
+    //UIImageView *soundLineView;
+    //UIImageView *startBarView;
+    //UIImageView *endBarView;
+    //NSMutableArray *viewInfoArray;
 }
+@property(nonatomic, assign) GraphViewSegment *current;
+@property (nonatomic, assign) UIViewController * parent;
+@property (nonatomic, retain) Song *currentSong;
+@property (nonatomic, retain) UIImageView *graphImageView;
+//@property (nonatomic, retain) NSMutableArray *viewInfoArray;
+@property CGFloat currentPixel;
+@property (nonatomic, retain) CALayer *bookMarkLayer;
+- (void)drawViewInfoArray;
+- (void)setCurrentPlaybackPosition: (int)pixel;
+- (void)setUpBookMarkLayer;
+- (void)settingStartEndTime: (CGFloat)start endPosition:(CGFloat)end;
+- (void)setDelta: (CGFloat)current_delta;
+
 
 @end
--- MusicWave/MyScrollView.m
+++ MusicWave/MyScrollView.m
@@ -7,60 +7,181 @@
 //
 
 #import "MyScrollView.h"
+#import "ViewInfo.h"
+#import "iPodSongsViewController.h"
+#import "BookMark.h"
+
+@interface GraphViewSegment : NSObject
+{
+	CALayer *layer;
+}
+@property(nonatomic, readonly) CALayer *layer;
+
+@end
+@implementation GraphViewSegment
+
+@synthesize layer;
+
+-(id)initWithFrame:(CGRect)frame
+{
+	self = [super init];
+	if(self != nil)
+	{
+        layer = [CALayer layer];
+        layer.frame = CGRectMake(0, 0, 1, frame.size.height);
+        layer.backgroundColor = [UIColor whiteColor].CGColor;
+        //layer.contents = (id) [UIImage imageNamed:@"bar.jpg"].CGImage;
+        layer.masksToBounds = YES;
+	}
+	return self;
+}
+
+-(void)dealloc
+{
+	[layer release];
+	[super dealloc];
+}
+
+@end
+
 
 
 @implementation MyScrollView
+@synthesize current, parent, currentPixel, currentSong, graphImageView;
+@synthesize bookMarkLayer;
+//@synthesize viewInfoArray;
 
-- (id)initWithFrame:(CGRect)frame
+#define myUpperMargin 0.0f
+
+
+// Designated initializer
+-(id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
+	self = [super initWithFrame:frame];
+	if(self != nil)
+	{
+        context = nil;
+        current = [[GraphViewSegment alloc] initWithFrame:self.bounds];
+        [self.layer addSublayer:current.layer];
+        bookMarkLayerDelegate = [[BookMarkLayerDelegate alloc] init];
+        bookMarkLayer = [CALayer layer];
+        bookMarkLayer.delegate = bookMarkLayerDelegate;
+        [self setUpBookMarkLayer];
+        [self.layer addSublayer:bookMarkLayer];
+        graphImageView = [[UIImageView alloc] initWithFrame:self.bounds];
+        [self addSubview:graphImageView];
+	}
+	return self;
 }
+
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.*/
+
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
-}
-*/
-- (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event {
-    //CGPoint pointInView = [[touches allObjects] locationInView:volumeView];
-    //NSLog(@"Start touches:pointInView x:%f, y:%f", pointInView.x, pointInView.y);
-    //UITouch *touch = [[touches allObjects] objectAtIndex:0];
-    //CGPoint touchPoint = [touch locationInView:touch.view];
-    //NSLog(@"[%s] touched point :: %f, %f", __FUNCTION__, touchPoint.x, touchPoint.y);
-    //[self setScrollEnabled:NO];
-    [[self.subviews objectAtIndex:0] touchesBegan:touches withEvent:event];
+    [super drawRect:rect];
+    if (context == nil) context = UIGraphicsGetCurrentContext();
     
+    //[self drawViewInfoArray];
+    
+    //endBarView.frame = CGRectMake(self.bounds.size.width - 2, 86, 2, 9);
+    
+    [bookMarkLayer setNeedsDisplay];
+    
+    [current.layer setPosition:CGPointMake(0.0 + current.layer.bounds.size.width / 2, current.layer.bounds.size.height / 2)];
+}
+- (void)setUpBookMarkLayer {
+    
+    bookMarkLayer.frame = CGRectMake(0.0, 0.0, self.contentSize.width, self.contentSize.height);
+    
+    bookMarkLayer.backgroundColor = [UIColor clearColor].CGColor;
+    bookMarkLayer.opacity = 1.0f;
+    bookMarkLayerDelegate.currentSong = self.currentSong;
+    //soundLineView.frame = CGRectMake(0, 89, self.bounds.size.width, 2);
+    
+    
+}
+- (void)setDelta: (CGFloat)current_delta {
+    NSLog(@"Settng BookMark layer delta:%f", current_delta);
+    bookMarkLayerDelegate.currentDelta = current_delta;
+}
+
+- (void)settingStartEndTime:(CGFloat)start endPosition:(CGFloat)end
+{
+    bookMarkLayerDelegate.startTime = start;
+    bookMarkLayerDelegate.endTime = end;
+}
+- (void) setCurrentPlaybackPosition:(int)pixel {
+    self.currentPixel = pixel;
+    //NSLog(@"Now currentPixel:%f", self.currentPixel);
+    [current.layer setPosition:CGPointMake(self.currentPixel, current.layer.bounds.size.height / 2)];
+}
+- (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event {
+    CGPoint pointInView = [[touches anyObject] locationInView:self];
+    //NSLog(@"Start touches:pointInView x:%f, y:%f", pointInView.x, pointInView.y);
+    
+    
+    if (self.contentSize.width < pointInView.x) {
+        NSLog(@"something wrong with view info array");
+        return;
+    }
+    self.currentPixel = pointInView.x;
+    [current.layer setPosition:CGPointMake(self.currentPixel, self.current.layer.bounds.size.height/2)];
+    //Get duration of the point and send it to viewcontroller
+    iPodSongsViewController *controller = (iPodSongsViewController *)parent;
+    //ViewInfo *tempViewInfo = [self.viewInfoArray objectAtIndex:self.currentPixel];
+    //NSLog(@"touches current time:%f", [tempViewInfo.time floatValue]);
+    [controller setCurrentPostion:self.currentPixel * controller.delta];
 }
 - (void)touchesMoved:(NSSet*)touches withEvent:(UIEvent*)event
 {
-    //UITouch *touch = [[touches allObjects] objectAtIndex:0];
-    //CGPoint touchPoint = [touch locationInView:touch.view];
-    //NSLog(@"[%s] touched point :: %f, %f", __FUNCTION__, touchPoint.x, touchPoint.y);
-    [[self.subviews objectAtIndex:0] touchesMoved:touches withEvent:event];
-
+    //CGPoint pointInView = [[touches anyObject] locationInView:self];
+    //NSLog(@"Moved pointInView x:%f, y:%f", pointInView.x, pointInView.y);
+    
 }
 
 - (void)touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event
 {
-    //UITouch *touch = [[touches allObjects] objectAtIndex:0];
-    //CGPoint touchPoint = [touch locationInView:touch.view];
-    //NSLog(@"[%s] touched point :: %f, %f", __FUNCTION__, touchPoint.x, touchPoint.y);
-    //[self setScrollEnabled:YES];
-    [[self.subviews objectAtIndex:0] touchesEnded:touches withEvent:event];
+    //CGPoint pointInView = [[touches anyObject] locationInView:self];
+    //NSLog(@"Ended pointInView x:%f, y:%f", pointInView.x, pointInView.y);
+    
+}
 
+- (void)drawViewInfoArray {
+    CGRect viewRect = self.bounds;
+    int len = 0;
+    
+    //CGContextSetRGBStrokeColor(context, 59, 65, 83, 1.0);
+    //CGContextMoveToPoint(context, 0.0, 1.0);
+    //CGContextAddLineToPoint(context, viewRect.size.width, 1.0);
+    //CGContextStrokePath(context);//upper stretching bar
+    
+    while( len < viewRect.size.width){
+        CGContextSetRGBStrokeColor(context, 54.0/255.0f, 57.0/255.0f, 65.0/255.0f, 1.0);
+        CGContextMoveToPoint(context, len, 0.0);
+        CGContextAddLineToPoint(context, len, 4.0);
+        CGContextStrokePath(context);//upper stretching bar
+        
+        if ((len % 25) == 0) {
+            CGContextSetRGBStrokeColor(context, 75.0/255.0f, 77.0/255.0f, 86.0/255.0f, 1.0);
+            CGContextMoveToPoint(context, len, 0.0);
+            CGContextAddLineToPoint(context, len, 8.0);
+            CGContextStrokePath(context);//upper stretching bar
+        }
+        len += 5;
+    }
+    //NSLog(@"view width:%f, view height:%f, graph height:%f", viewRect.size.width, viewRect.size.height, graphHeight);
 }
 
 
 - (void)dealloc
 {
+    [current release];
+    [bookMarkLayerDelegate release];
+    [currentSong release];
+    [graphImageView release];
     [super dealloc];
 }
-
 @end
